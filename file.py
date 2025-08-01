0b4b819fec6c06fbb18df57f805ecf8a962f917d
segni_abera <segniab69@gmail.com>
2025-08-01 22:16:12 +0300

    file operations in python

---
+++ file.py
@@ -0,0 +1,18 @@
+try:
+    file_name = input("enter the name of the file: " "")
+    with open(file_name, 'r') as file:
+        data = file.read()
+        print(data)
+        count = len(data.split())   
+        print(f"Number of words: {count}")
+        upper = data.upper()
+        print("Uppercase version:", upper)
+    
+    
+    with open("output.txt", 'w') as output_file:
+        output_file.write(f"Number of words: {count}\n")
+        output_file.write(f"Uppercase version: {upper}\n")
+
+except FileNotFoundError:
+    print("file not found ")
+       
